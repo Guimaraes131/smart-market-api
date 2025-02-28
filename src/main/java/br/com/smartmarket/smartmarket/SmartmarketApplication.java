cbaaf0fb3a6b2416e3395d5ca61cb5df87d6df01
guilhos <guilhermegpereira26@hotmail.com>
2025-02-28 10:23:31 -0300

    iniciando projeto Spring

---
+++ src/main/java/br/com/smartmarket/smartmarket/SmartmarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartmarket.smartmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmarketApplication.class, args);
+	}
+
+}
