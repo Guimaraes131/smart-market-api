4218a646aff709b8203d096c948c5e270f8f6d16
Guilherme <guilhermegpereira26@hotmail.com>
2025-04-03 18:27:44 -0300

    Add CorsConfig, Repository interface, ValidationHandler

---
+++ src/main/java/br/com/smartmarket/smartmarket/exception/ValidationHandler.java
@@ -0,0 +1,26 @@
+package br.com.smartmarket.smartmarket.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+public class ValidationHandler {
+    record ValidationErrorMessage(String field, String message) {
+        public ValidationErrorMessage(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public List<ValidationErrorMessage> handle(MethodArgumentNotValidException e) {
+        return e.getFieldErrors()
+                .stream()
+                .map(ValidationErrorMessage::new)
+                .toList();
+    }
+}
---
+++ src/main/java/br/com/smartmarket/smartmarket/repository/SetorRepository.java
@@ -0,0 +1,7 @@
+package br.com.smartmarket.smartmarket.repository;
+
+import br.com.smartmarket.smartmarket.model.Setor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetorRepository extends JpaRepository<Setor, Long> {
+}
