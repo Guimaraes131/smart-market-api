630dd1e76f920c02b04408dfb931ec14674ff308
Guilherme <guilhermegpereira26@hotmail.com>
2025-04-03 23:14:59 -0300

    Adicionando anotacao

--- src/main/java/br/com/smartmarket/smartmarket/exception/ValidationHandler.java
+++ src/main/java/br/com/smartmarket/smartmarket/exception/ValidationHandler.java
@@ -5,9 +5,11 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+@RestControllerAdvice
 public class ValidationHandler {
     record ValidationErrorMessage(String field, String message) {
         public ValidationErrorMessage(FieldError fieldError) {
